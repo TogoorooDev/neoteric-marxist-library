c249f2bb76ebcee7aecbed4e58045a25b7685ee7
TogoorooDev <robhensley10@gmail.com>
2020-12-08 20:46:33 -0800

    added import

--- site/js/index.js
+++ site/js/index.js
@@ -1,4 +1,5 @@
 async function main(){
+	const netlify = import NetlifyClient
 	const client = new netlify.NetlifyAPI("NDlT3RgxN4y2e8fwlxg28OUvwXphzoys7KrjvA0Y_yY");
 	const sites = await client.listSites();
 	console.log(sites);
