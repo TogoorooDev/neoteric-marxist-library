b244be6397acc1d9cea97441d438aee451088a7f
TogoorooDev <robhensley10@gmail.com>
2020-12-08 20:28:26 -0800

    fixed script tag

--- site/js/index.js
+++ site/js/index.js
@@ -1,7 +1,7 @@
-// async function main(){
-// 	const client = new netlify.NetlifyAPI("NDlT3RgxN4y2e8fwlxg28OUvwXphzoys7KrjvA0Y_yY");
-// 	const sites = await client.listSites();
-// 	console.log(sites);
-// }
+async function main(){
+	const client = new netlify.NetlifyAPI("NDlT3RgxN4y2e8fwlxg28OUvwXphzoys7KrjvA0Y_yY");
+	const sites = await client.listSites();
+	console.log(sites);
+}
 
-// main();
+main();
