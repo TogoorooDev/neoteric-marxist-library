f2914dc79bfc97286643d2934172f508d7d4c070
TogoorooDev <robhensley10@gmail.com>
2020-12-08 18:50:23 -0800

    attemped to fix api issues

--- site/js/index.js
+++ site/js/index.js
@@ -1,5 +1,5 @@
 async function main(){
-	const client = new NetlifyAPI("NDlT3RgxN4y2e8fwlxg28OUvwXphzoys7KrjvA0Y_yY");
+	const client = new netlify.NetlifyAPI("NDlT3RgxN4y2e8fwlxg28OUvwXphzoys7KrjvA0Y_yY");
 	const sites = await client.listSites();
 	console.log(sites);
 }
